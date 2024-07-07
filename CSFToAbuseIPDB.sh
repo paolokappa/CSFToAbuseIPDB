8e5e2927cb9705b8f7cc751d69098584e501dcb5
Paolo Caparrelli <kappa@goline.ch>
2024-07-08 01:18:34 +0200

    CSFToAbuseIPDB.sh bash script

--- CSFToAbuseIPDB.sh
+++ CSFToAbuseIPDB.sh
@@ -9,7 +9,7 @@ determine_category() {
     case "$trigger" in
         LF_SSHD) echo "22,18" ;; # SSH
         LF_TRIGGER) echo "22,18" ;; # SSH Brute-Force
-        LF_DISTATTACK) echo "4" ;; # DDoS attack
+        LF_DISTATTACK) echo "4,22" ;; # DDoS attack
         LF_DISTFTP) echo "5" ;; # FTP Brute-Force
         LF_WEBMIN_EMAIL_ALERT) echo "15,21" ;; # Webmin attack
         LF_EMAIL_ALERT) echo "11,18" ;; # Email attack
@@ -41,9 +41,20 @@ fi
 
 # Loop through all unique IPs and send the data to AbuseIPDB
 for ip in $ips; do
-    # Extract relevant logs for the current IP
-    ip_logs=$(echo "$logs" | grep "$ip")
-    comment="${message}; Ports: ${ports}; Direction: ${direction}; Trigger: ${trigger}; Logs: ${ip_logs}"
+    if [ "$trigger" = "LF_DISTATTACK" ]; then
+        # Count the number of distributed attacks
+        attack_count=$(echo "$logs" | grep -c "$ip")
+        # Extract relevant logs for the current IP
+        ip_logs=$(echo "$logs" | grep "$ip")
+        # Construct the comment string for LF_DISTATTACK
+        comment="Detected $attack_count distributed attacks from $ip. LF_DISTATTACK; Logs: $(echo "$ip_logs" | tr '\n' ' ')"
+    else
+        # Extract relevant logs for the current IP
+        ip_logs=$(echo "$logs" | grep "$ip")
+        # Construct the comment string for other triggers
+        comment="${message}; IP: ${ip}; Ports: ${ports}; Direction: ${direction}; Trigger: ${trigger}; Logs: $(echo "$ip_logs" | tr '\n' ' ')"
+    fi
+    
     echo "Reporting IP: $ip with comment: $comment"
     curl https://api.abuseipdb.com/api/v2/report \
       --data-urlencode "ip=$ip" \
